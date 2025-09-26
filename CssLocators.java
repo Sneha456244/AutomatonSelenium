afad7c9006443f816ed4b1a3d18f60f5250304c3
Sneha456244 <151616854+Sneha456244@users.noreply.github.com>
2025-09-26 15:50:21 +0530

    Create CssLocators.java

---
+++ CssLocators.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CssLocators {
+
+	public static void main(String[]args) {
+		
+		WebDriver driver=new ChromeDriver();
+		
+		//get URL
+		driver.get("https://demo.nopcommerce.com/");
+		driver.manage().window().maximize(); //maximize the browser window
+		
+		//tag id       tag#id
+		//driver.findElement(By.cssSelector("input#small-searchterms")).sendKeys("T-shirts"); 
+		//driver.findElement(By.cssSelector("#small-searchterms")).sendKeys("T-shirts"); //without Tag
+		
+		//tag class     tag.class
+		//driver.findElement(By.cssSelector("input.search-box-text")).sendKeys("T-shirts"); 
+		//driver.findElement(By.cssSelector(".search-box-text")).sendKeys("T-shirts"); 
+		
+		//tag attribute tag[attribute='Value']
+		//driver.findElement(By.cssSelector("input[placeholder=\"Search store\"]")).sendKeys("T-shirts");
+		//driver.findElement(By.cssSelector("[placeholder=\"Search store\"]")).sendKeys("T-shirts");
+		
+		//tag class attribute
+		driver.findElement(By.cssSelector("input.search-box-text[name='q'")).sendKeys("T-shirts");
+		
+		
+		
+	}
+
+}
