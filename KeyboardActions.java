6db9b936523fd341d37bd2d53dbe883a1b35a93d
Sneha456244 <151616854+Sneha456244@users.noreply.github.com>
2025-10-09 17:35:44 +0530

    Create KeyboardActions.java

---
+++ KeyboardActions.java
@@ -0,0 +1,41 @@
+package Day11half;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class KeyboardActions {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://text-compare.com/");
+		driver.manage().window().maximize();
+		
+		Actions act=new Actions(driver);
+		
+		driver.findElement(By.xpath("//textarea[@id='inputText1']")).sendKeys("WELCOME");
+		
+		//Ctrl+A
+		act.keyDown(Keys.CONTROL).sendKeys("A").keyUp(Keys.CONTROL).perform();
+		
+		//Ctrl+C
+		act.keyDown(Keys.CONTROL).sendKeys("C").keyUp(Keys.CONTROL).perform();
+		
+		//Tab
+		act.keyDown(Keys.TAB).keyUp(Keys.TAB).perform();
+		
+		Thread.sleep(3000);
+		
+		//Ctrl+V
+		act.keyDown(Keys.CONTROL).sendKeys("V").keyUp(Keys.CONTROL).perform();
+		
+	}
+
+}
