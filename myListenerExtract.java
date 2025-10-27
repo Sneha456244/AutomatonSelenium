b450266b5e467493e7abe719a1ac568f3c227e6f
Sneha456244 <151616854+Sneha456244@users.noreply.github.com>
2025-10-27 17:48:48 +0530

    Create myListenerExtract.java

---
+++ myListenerExtract.java
@@ -0,0 +1,29 @@
+package TestNG04AF;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class myListener2 implements ITestListener {
+	
+	public void onStart(ITestContext context) {
+		System.out.println("Test Execution is started....");
+	  }
+	public void onTestStart(ITestResult result) {
+		System.out.println("Test Started");
+	  }
+	public void onTestSuccess(ITestResult result) {
+		System.out.println("Test Passed");
+	  }
+	public void onTestFailure(ITestResult result) {
+		System.out.println("Test Failed");
+	  }
+	public void onTestSkipped(ITestResult result) {
+		System.out.println("Test Skipped");
+	  }
+	public void onFinish(ITestContext context) {
+		System.out.println("Test Execution is Finished....");
+	  }
+	
+
+}
