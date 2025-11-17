848402bea5970d8198fea1afcd573e9b9c0fb653
Sneha456244 <151616854+Sneha456244@users.noreply.github.com>
2025-11-17 17:14:28 +0530

    Create Ad-DashboardPage.java

---
+++ Ad-DashboardPage.java
@@ -0,0 +1,26 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+ 
+public class DashboardPage {
+    WebDriver driver;
+
+    public DashboardPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath="//h6[text()='Dashboard']")
+    WebElement dashboardTitle;
+
+    public boolean isDashboardDisplayed() {
+        try {
+            return dashboardTitle.isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
